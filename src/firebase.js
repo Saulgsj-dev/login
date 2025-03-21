d03282387141f6ee3d1181189201b83909354c16
Saulgsj <saulgrong@gmail.com>
2025-03-21 18:06:53 -0300

    Protegendo chaves do Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDRmBwLiLW8gV-GIl0LH489oyaRDutZ8rg",
-    authDomain: "login-simples-aca21.firebaseapp.com",
-    projectId: "login-simples-aca21",
-    storageBucket: "login-simples-aca21.firebasestorage.app",
-    messagingSenderId: "279822341155",
-    appId: "1:279822341155:web:896d8c8d490fe27f0e4d96"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
